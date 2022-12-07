93396019c9b26d00f27f1509d3757d3fd15f0c5c
0xTadash1 <56265995+0xTadash1@users.noreply.github.com>
2022-12-07 20:45:02 +0900

    feat: add `xdg-symlinks.sh`

---
+++ xdg-symlinks.sh
@@ -0,0 +1,10 @@
+xdg-symlinks() {
+	local d
+	for d in '.local' '.cache' '.config'; do
+		if [ -d "$d" ]; then
+			echo "directory $d is already exist."
+			continue
+		fi
+		'command' 'ln' -s "xdg/$d" "./$d"
+	done
+}
