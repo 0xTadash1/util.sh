c2d76dd45c991f47fe0bda73848c665ce35298f5
0xTadash1 <56265995+0xTadash1@users.noreply.github.com>
2023-01-05 03:30:02 +0900

    chore: refactoring

--- btrfs-prop-set-user-dirs-comp.sh
+++ btrfs-prop-set-user-dirs-comp.sh
@@ -1,6 +1,6 @@
 btrfs-prop-set-user-dirs-comp() (
-  _err() { echo 'ERR: '"$@" >/dev/null 1>&2; }
-  _info() { echo 'INFO: '"$@"; }
+  _err() { printf 'ERR: %s\n' "$@" >/dev/null 1>&2; }
+  _info() { printf 'INFO: %s\n' "$@"; }
 
   # Load all `XDG_xxxx_DIR` vars
   userdirs_dirs="$XDG_CONFIG_HOME/user-dirs.dirs"
@@ -18,26 +18,26 @@ btrfs-prop-set-user-dirs-comp() (
     no|none) algo='none' ;;
     '') algo='' ;;
     *)
-      _err 'expected to take the compression algorithm as an argument:'
-      _err '  - zlib: slower, higher compression ratio'
-      _err '  - lzo : designed to be fast, worse compression ratio than zlib'
-      _err '  - zstd: similar compression ratio to zlib, but faster'
-      _err '  - no or none       : disable compression (same as `chattr +m`)'
-      _err '  - "" (empty string): set the dafault value'
-      _err ''
-      _err '  In this way, the compression level is not supported.'
-      _err ''
-      _err '  cf. https://btrfs.wiki.kernel.org/index.php/Compression'
-      _err '  cf. btrfs-property(8) or https://btrfs.readthedocs.io/en/latest/btrfs-property.html'
+      _err 'expected to take the compression algorithm as an argument:' \
+           '  - zlib: slower, higher compression ratio' \
+           '  - lzo : designed to be fast, worse compression ratio than zlib' \
+           '  - zstd: similar compression ratio to zlib, but faster' \
+           '  - no or none       : disable compression (same as `chattr +m`)' \
+           '  - "" (empty string): set the dafault value' \
+           '' \
+           '  In this way, the compression level is not supported.' \
+           '' \
+           '  cf. https://btrfs.wiki.kernel.org/index.php/Compression' \
+           '  cf. btrfs-property(8) or https://btrfs.readthedocs.io/en/latest/btrfs-property.html'
       exit 2
       ;;
   esac
 
-  _info 'The compressions works on data written.'
-  _info 'Therefore, the properties should be set on an empty file/directory.'
+  _info 'The compressions works on data written.' \
+        'Therefore, the properties should be set on an empty file/directory.'
   echo
 
-  printf '%s' 'Start setting properties with `'"$algo"'` as the algorithm? [Y/n]: '
+  printf '%s' "Start setting properties with \`$algo\` as the algorithm? [Y/n]: "
   read _rep
   case "$_rep" in
     ''|[yY]*) true ;;
@@ -55,7 +55,7 @@ btrfs-prop-set-user-dirs-comp() (
       btrfs property set "$user_dir" compression "$algo" \
         || exit $?
     else
-      _info "\`$user_dir\` not found. Passed."
+      _info "\`$user_dir\` not found. Skipped."
     fi
   }
 
